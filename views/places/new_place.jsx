f0975eb5fb0f797bcb05d136891a7a0e31c8c490
Samuel Fisher <skiy23@outlook.com>
2022-03-10 00:52:53 -0800

    Bonus

---
+++ views/places/new_place.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function new_place () {
+    return (
+        <Def>
+            <main>
+                <h1>ADD NEW PLACE</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = new_place
---
+++ views/places/show_place.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function show_place () {
+    return (
+        <Def>
+            <main>
+                <h1>SHOW SELECTED PLACE</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = show_place
