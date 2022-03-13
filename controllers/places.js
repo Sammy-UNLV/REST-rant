a3c3cc97e3fa577c841087d6820d4717874bee7e
Samuel Fisher <skiy23@outlook.com>
2022-03-13 03:30:18 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,32 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 //GET /places
 router.get('/', (req, res)=> {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'images/patrick-tomasso-GXXYkSwndP4-unsplash.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'images/shawnanggg-nmpW_WwwVSc-unsplash.jpg'
-      }]
     res.render('places/index', {places})
 })
 
-module.exports = router
+// POST 
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
 
 // NEW
 router.get('/new', (req, res) => {
   res.render('places/new')
 }) 
 
+
+module.exports = router
