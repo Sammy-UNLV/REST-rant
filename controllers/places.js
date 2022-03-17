10062b5c7984a1d94c5fe775961d3a89a1fb9c76
Samuel Fisher <skiy23@outlook.com>
2022-03-17 13:40:39 -0700

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -4,7 +4,7 @@ const places = require('../models/places.js')
 
 //GET /places
 router.get('/', (req, res)=> {
-    res.render('places/index', {places})
+  res.render('places/index', {places})
 })
 
 // POST 
@@ -29,36 +29,6 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 }) 
 
-// DELETE
-router.delete('/places/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id,1)
-    res.redirect('/places')
-  }
-})
-
-// SHOW
-router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    
-    res.render('places/show', { place: places[id], id })
-  }
-})
-
 // EDIT
 router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
@@ -72,5 +42,67 @@ router.get('/:id/edit', (req, res) => {
     res.render('places/edit', { place: places[id] })
   }
 })
+
+// SHOW
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+
+    res.render('places/show', { place: places[id], id})
+  }
+})
+
+
+// DELETE
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id,1)
+    res.redirect('/places')
+  }
+})
+
+// PUT
+router.put('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
+
 
 module.exports = router
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,12 +1,11 @@
 const React = require('react')
 const Def = require('../default')
 
-function edit_form(data) {
+function edit_form (data) {
     return (
         <Def>
             <main>
-                <h1>Add New Place</h1>
-                <form method="POST" action={`/places/${data.id}?_methiood=PUT`}>
+            <form method="POST" action={`/places/${data.id}?_method=PUT`}>
                     <div className="row">
                         <div className="form-group col-sm-6">
                             <label htmlFor="name">Place Name</label>
